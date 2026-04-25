ec890bff277e6ae2502a749fe9047e9618e57684
jadzeino <ahmad.zeino@gmail.com>
2026-04-25 12:38:55 +0200

    fix MUI barrel imports in design-system, tidy up Button

--- packages/design-system/src/components/Button/index.tsx
+++ packages/design-system/src/components/Button/index.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
-import { AutoAwesome } from '@mui/icons-material';
-import { ButtonProps } from '@mui/material/Button';
+import type { ButtonProps } from '@mui/material/Button';
 import * as S from './style';
 
-export const Button: React.FC<ButtonProps> = ({ ...props }) => (
-  <S.Button {...props} autoFocus onClick={() => undefined}>
-    <AutoAwesome /> {props.children}
-  </S.Button>
-);
+export const Button: React.FC<ButtonProps> = (props) => <S.Button {...props} />;
--- packages/design-system/src/components/Button/style.ts
+++ packages/design-system/src/components/Button/style.ts
@@ -1,5 +1,5 @@
-import { Button as MuiButton } from '@mui/material';
-import { styled } from '@mui/material';
+import MuiButton from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
 import { spacing } from '@raisin/design-tokens';
 
 export const Button = styled(MuiButton)({
